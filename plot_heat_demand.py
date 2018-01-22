38520f97fe61ae3deb1a343a601861627100c58a
caroline.moeller <caroline.moeller@rl-institut.de>
2018-01-22 14:52:46 +0100

    Expand plotting function

--- plot_heat_demand.py
+++ plot_heat_demand.py
@@ -13,8 +13,24 @@ import matplotlib.pyplot as plt
 full_filename = os.path.join(os.path.dirname(__file__),
     'heat_demand.csv')
 data = pd.read_csv(full_filename, sep=",")
-print(data)
+data_dh = data.drop(['DH_DE_Rem', 'DH_DE_Ind'], axis=1)
+data_dh_normalized = data_dh/data_dh.max()
 
+plt.figure(figsize=(12,8))
 plt.plot(data)
+plt.title('heat demand of 10 district heating systems' +
+        '+ rest of Germany (pink) + industry (grey)')
 plt.legend(data.columns.values, loc='upper center')
+
+plt.figure(figsize=(12,8))
+plt.plot(data_dh)
+plt.title('heat demand of 10 district heating systems')
+plt.legend(data_dh.columns.values, loc='upper center')
+
+
+plt.figure(figsize=(12,8))
+plt.plot(data_dh_normalized)
+plt.title('heat demand of 10 district heating systems normalized')
+plt.legend(data_dh_normalized.columns.values, loc='upper center')
+
 plt.show()
