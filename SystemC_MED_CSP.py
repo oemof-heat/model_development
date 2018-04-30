8f1b76b000732fc85a5b307f9830d225777512f6
Franziska Pleissner <franziska.pleissner@beuth-hochschule.de>
2018-04-30 15:43:50 +0200

    Remove unnecessary transformer and change the constrains part

--- SystemC_MED_CSP.py
+++ SystemC_MED_CSP.py
@@ -15,10 +15,12 @@ desalination and concentrating solar power plant
 # Import packages
 from oemof.tools import logger
 import oemof.solph as solph
+import pyomo.environ as po
 
 import oemof.outputlib as outputlib
 
 import logging
+import time
 import pandas as pd
 
 # import oemof plots
@@ -27,7 +29,7 @@ try:
 except ImportError:
     plt = None
 
-number_of_time_steps = 8760
+number_of_time_steps = 500
 
 # initiate the logger
 logger.define_logging(logfile='oemof_example.log',
@@ -50,53 +52,53 @@ energysystem = solph.EnergySystem(timeindex=date_time_index)
 # Build up the system #
 #######################
 
-# Create busses
+### Buses ###
+
+# Create electrical bus and water bus
 bel = solph.Bus(label='electricity')
 bwat = solph.Bus(label='water')
-bsol = solph.Bus(label='solarenergy')
 # Create 2 thermal busses, one with high, one with low temperature
 bthl = solph.Bus(label='thermal_low')
 bthh = solph.Bus(label='thermal_high')
 # Add the buses to the energy system
-energysystem.add(bwat, bel, bthl, bthh, bsol)
+energysystem.add(bwat, bel, bthl, bthh)
+
+### Sinks and Sources ###
 
 # Add demand of water and electric sink
 energysystem.add(solph.Sink(label='demand', inputs={bwat: solph.Flow(
         fixed=True, actual_value=data['demand_wat'], nominal_value=61472.3)}))
 energysystem.add(solph.Sink(label='elec_output', inputs={bel: solph.Flow(
-        variable_costs=-4)}))
-# excess sink  water
+        variable_costs=-0.04)}))
+# Add excess sinks
 energysystem.add(solph.Sink(label='excesswat', inputs={bwat: solph.Flow()}))
+energysystem.add(solph.Sink(label='excessthh', inputs={bthh: solph.Flow()}))
+energysystem.add(solph.Sink(label='excessthl', inputs={bthl: solph.Flow()}))
 
 # Add energy source
-energysystem.add(solph.Source(label='solar', outputs={bsol: solph.Flow(
-        fixed=True, actual_value=data['pv'],
-        investment=solph.Investment(ep_costs=70.64))}))
+energysystem.add(solph.Source(label='solar', outputs={bthh: solph.Flow(
+        fixed=True, actual_value=data['pv'], investment=solph.Investment(ep_costs=529.05))}))
+# Add shortage sources, should be 0, just there to avoid an underdetermined system
 energysystem.add(solph.Source(label='Wat_shortage', outputs={bwat: solph.Flow(
-        variable_costs=10000)}))      # should be 0, just there to avoid an underdetermined system
+        variable_costs=10000)}))
 energysystem.add(solph.Source(label='thh_shortage', outputs={bthh: solph.Flow(
         variable_costs=10000)}))
 
-# Add Transformers
-# Collector
-Col = solph.Transformer(
-        label='Collector',
-        inputs={bsol: solph.Flow()},
-        outputs={bthh: solph.Flow(investment=solph.Investment(ep_costs=529.05))},
-        conversion_factors={bthh: 1})
+### Transformers ###
 
 # Power-Block
 PB = solph.Transformer(
         label='PB',
-        inputs={bthh: solph.Flow()},
-        outputs={bthl: solph.Flow(investment=solph.Investment(ep_costs=0.0001)),
+        inputs={bthh: solph.Flow(investment=solph.Investment(ep_costs=0.0001))},
+        outputs={bthl: solph.Flow(),
                  bel: solph.Flow()},
         conversion_factors={bthl: 0.714, bel: 0.286})
+
 # Transformer to show the energy flow from the collectors to the desalination
 TT = solph.Transformer(
         label='TT',
         inputs={bthh: solph.Flow()},
-        outputs={bthl: solph.Flow(investment=solph.Investment(ep_costs=0))},
+        outputs={bthl: solph.Flow(investment=solph.Investment(ep_costs=0.0001))},
         conversion_factors={bthl: 1})
 
 # Desalination
@@ -108,9 +110,10 @@ Desal = solph.Transformer(
         conversion_factors={bthl: 0.981595, bel: 0.018405,
                             bwat: 0.01227})
 
-energysystem.add(PB, TT, Desal, Col)
+energysystem.add(PB, TT, Desal)
 
-# Create Storages
+### Storages ###
+
 storage_thh = solph.components.GenericStorage(
                 label='storage_thh',
                 inputs={bthh: solph.Flow()},
@@ -120,7 +123,7 @@ storage_thh = solph.components.GenericStorage(
                 nominal_output_capacity_ratio=1/6,
                 inflow_conversion_factor=0.9,
                 outflow_conversion_factor=0.9,
-                investment=solph.Investment(ep_costs=0.001)
+                investment=solph.Investment(ep_costs=0.0001)
                 )
 
 storage_wat = solph.components.GenericStorage(
@@ -128,12 +131,11 @@ storage_wat = solph.components.GenericStorage(
                 inputs={bwat: solph.Flow()},
                 outputs={bwat: solph.Flow()},
                 capacity_loss=0,
-                initial_capacity=0,
                 nominal_input_capacity_ratio=1/6,
                 nominal_output_capacity_ratio=1/6,
                 inflow_conversion_factor=1,
                 outflow_conversion_factor=1,
-                investment=solph.Investment(maximum=100000, ep_costs=6.54)
+                investment=solph.Investment(maximum=200000, ep_costs=6.54)
                 )
 
 storage_el = solph.components.GenericStorage(
@@ -146,7 +148,7 @@ storage_el = solph.components.GenericStorage(
                 nominal_output_capacity_ratio=1/6,
                 inflow_conversion_factor=1,
                 outflow_conversion_factor=0.95,
-                investment=solph.Investment(maximum=750000, ep_costs=15.57)
+                investment=solph.Investment(maximum=1500000, ep_costs=15.57)
                 )
 
 energysystem.add(storage_thh, storage_wat, storage_el)
@@ -157,29 +159,27 @@ energysystem.add(storage_thh, storage_wat, storage_el)
 
 # Initialise the operational model (create problem) with constrains
 om = solph.Model(energysystem)
-ColGr = energysystem.groups['Collector']
-PBGr = energysystem.groups['PB']
-SpGr = energysystem.groups['storage_thh']
-# nominal capacity (output) of the collectors = input of the Power Block:
-solph.constraints.equate_variables(
-    om,
-    om.InvestmentFlow.invest[ColGr, bthh],
-    om.InvestmentFlow.invest[PBGr, bthl],
-    factor1=0.714)
-# Storage of thermal energy: for 5h with max power
-solph.constraints.equate_variables(
-    om,
-    om.InvestmentFlow.invest[ColGr, bthh],
-    om.InvestmentFlow.invest[SpGr, bthh],
-    factor1=5)
 
+### Add own constrains ###
+# Get value for components withouta name
+SolGr = energysystem.groups['solar']
+# Create a block and add it to the system
+myconstrains = po.Block()
+om.add_component('MyBlock', myconstrains)
+# Add the constrains to the created block
+myconstrains.PB_size = po.Constraint(expr=(
+        om.InvestmentFlow.invest[SolGr, bthh] == om.InvestmentFlow.invest[bthh, PB]))
+myconstrains.storage_size = po.Constraint(expr=(
+        om.GenericInvestmentStorageBlock.invest[storage_thh] <= 5*om.InvestmentFlow.invest[bthh, PB]))
 
 # Set tee to True to get the solver output
 om.solve(solver='cbc', solve_kwargs={'tee': True})
 
 energysystem.results['main'] = outputlib.processing.results(om)
 energysystem.results['meta'] = outputlib.processing.meta_results(om)
+energysystem.results['param'] = outputlib.processing.param_results(om)
 
 # store the results to plot them in other file
+timestr = time.strftime("%Y%m%d-%H%M")
 energysystem.dump(dpath="C:\Git_clones\oemof_heat\Dumps",
-                  filename="MED_CSP.oemof")
+                  filename="MED_CSP_"+timestr+".oemof")
