1d9676307ad759597e5c00ce307ebe2f2723354f
jnnr <jlnr@web.de>
2018-08-23 16:29:58 +0200

    Add preparation of gas prices

--- System_B/time_series_preparation.py
+++ System_B/time_series_preparation.py
@@ -22,13 +22,17 @@ import os
 from workalendar.europe import Germany
 
 abs_path = os.path.dirname(os.path.abspath(__file__))
-filename = abs_path + '/data/' + 'ninja_weather_51.8341_12.2374_uncorrected.csv' # 'temperature_data.csv'
 
+# create synthetic heat profiles via BDEW method
+
+# load temperature data
+filename = abs_path + '/data/' + 'ninja_weather_51.8341_12.2374_uncorrected.csv' # 'temperature_data.csv'
 temperature = pd.read_csv(filename, skiprows=2)  # ["temperature"]
 temperature.columns = ['utc_time','time','temp']
 temperature = temperature[['time','temp']]
 temperature.set_index('time')
 
+# get holidays for germany
 cal = Germany()
 holidays = dict(cal.holidays(2010))
 holidays
@@ -58,6 +62,14 @@ demand['ghd'] = bdew.HeatBuilding(
     shlp_type='ghd', wind_class=0, annual_heat_demand=140000,
     name='ghd').get_bdew_profile()
 
-
-# save demand time series
+# save heat demand time series
 demand.to_csv(abs_path+'/data/'+'demand_heat.csv')
+
+# prepare gas price time series
+ger_day_ahead_prices_2006_2018 = pd.read_csv(abs_path +'/data/opsd-time_series-2018-06-30/time_series_60min_singleindex.csv', index_col='cet_cest_timestamp')['DE_price_day_ahead']
+print(ger_day_ahead_prices_2006_2018)
+ger_day_ahead_prices_2014 = ger_day_ahead_prices_2006_2018.loc['2014-01-01T00:00:00+0100':'2014-12-31T23:00:00+0100']
+ger_day_ahead_prices_2006_2018.to_csv(abs_path+'/data/'+'day_ahead_price_el_2006_2018.csv')
+ger_day_ahead_prices_2014.to_csv(abs_path+'/data/'+'day_ahead_price_el_2014.csv')
+
+# prepare electricity price time series
