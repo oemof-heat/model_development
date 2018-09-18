72dfe782f34f4a35d31b53da041b7ff4bd7d898c
jnnr <jlnr@web.de>
2018-09-18 18:38:24 +0200

    Add automatic report generation

--- System_B/src/main.py
+++ System_B/src/main.py
@@ -6,7 +6,7 @@ This script runs the whole workflow of the analysis
 from oemof.tools import logger
 import logging
 import os
-
+import subprocess
 
 from model_dessau import run_model_dessau
 import create_plots
@@ -26,7 +26,7 @@ logging.info('Load config file')
 
 # Run the optimisation model
 logging.info('Run optimisation model')
-run_model_dessau(config_path="/scenarios/config.yml")
+run_model_dessau(config_path="/experiments/experiment_1.yml")
 
 # Postprocessing
 logging.info('Postprocess data')
@@ -35,6 +35,12 @@ logging.info('Postprocess data')
 logging.info('Create plots')
 create_plots.create_plots()
 
-# Rebuild the reports
+# Create a table of the scenario
 
 
+# Build the report
+cmd = ['pdflatex', '-interaction=nonstopmode', '--output-directory={0}/presentation/build'.format(abs_path), '{0}/presentation/report.tex'.format(abs_path)]
+process = subprocess.call(cmd) # , stdout=open(os.devnull, 'wb'))
+# cmd = ['pdflatex', '-interaction', 'nonstopmode', 'cover.tex']
+# proc = subprocess.Popen(cmd)
+# proc.communicate()
