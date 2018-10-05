f5250f82234d7ff014cad543424e7a38df17d857
jnnr <jlnr@web.de>
2018-10-05 12:03:35 +0200

    Separate postprocessing from plot

---
+++ System_B/src/postprocess.py
@@ -0,0 +1,40 @@
+"""
+Determine the following quantities on each technology and save them in a dataframe:
+
+Variable costs
+Summed operating hours
+Summed Production
+Mean Production during operation hours
+Maximal Production
+Minimal Production
+Full load hours
+Start count
+
+
+The following quantities concern the energy system as a whole:
+
+Coverage through renewables
+Summed Excess, max Excess
+Summed import, max import
+"""
+
+
+energysystem = solph.EnergySystem()
+energysystem.restore(dpath=abs_path + '/results', filename='es.dump')
+
+
+def get_variable_costs():
+def get_summed operating hours():
+def get_summed Production():
+def get_mean Production during operation hours():
+def get_maximal Production():
+def get_minimal Production():
+def get_full load hours():
+def get_start count():
+
+def get_coverage_through_renewables():
+def get_summed_excess():
+def get_max_excess():
+def get_summed_import():
+def get_max_import():
+
