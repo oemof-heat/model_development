43e349c24424dba7a9cef08f522c23e67533ebbd
Jakob Wolf <jakob.wolf@beuth-hochschule.de>
2018-11-22 16:46:31 +0100

    Update model

--- System_A/src/model_flex_chp.py
+++ System_A/src/model_flex_chp.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
 """
+
+Date: 22nd of November 2018
+Author: Jakob Wolf (jakob.wolf@beuth-hochschule.de)
+
 General description
 -------------------
 
@@ -70,15 +74,13 @@ import networkx as nx
 import logging
 import os
 import pandas as pd
-import yaml
+import yaml  # pip install pyyaml
 import pprint as pp
-import timeit
-start_time = timeit.default_timer()
 
 
 def run_model_flexchp(config_path):
 
-    with open(abs_path + config_path, 'r') as ymlfile:
+    with open(config_path, 'r') as ymlfile:
         cfg = yaml.load(ymlfile)
 
     if cfg['debug']:
@@ -88,38 +90,36 @@ def run_model_flexchp(config_path):
 
     # solver = 'cbc'
     solver = cfg['solver']
-    debug = False  # Set number_of_timesteps to 3 to get a readable lp-file.
+    debug = cfg['debug']
     periods = number_of_time_steps
-    solver_verbose = True  # show/hide solver output
+    solver_verbose = cfg['solver_verbose']  # show/hide solver output
 
     # initiate the logger (see the API docs for more information)
-    logger.define_logging(logfile='flexCHB.log',
+    logger.define_logging(logfile=cfg['filename_logfile'],
                           screen_level=logging.INFO,
                           file_level=logging.DEBUG)
 
     logging.info('Initialize the energy system')
-    date_time_index = pd.date_range('1/1/2030', periods=number_of_time_steps,
-                                    freq='H')
+    date_time_index = pd.date_range(cfg['start_date'], periods=number_of_time_steps,
+                                    freq=cfg['frequency'])
 
     energysystem = solph.EnergySystem(timeindex=date_time_index)
 
-    # Read data file
+    ##########################################################################
+    # Read time series and parameter values from data files
+    ##########################################################################
+
     abs_path = os.path.dirname(os.path.abspath(os.path.join(__file__, '..')))
-    filename = abs_path + '/data_raw/data_confidential/demand_profile_A_nominal_20180912.csv'
-    #abs_path = 'C:/Users/jwolf/Documents/oemof_heat/System_A '
-    # try:
-    #     filename = os.path.join(os.path.dirname(__file__), 'demand_profile_A_nominal_20180912.csv')
-    # except:
-    #     print('ERROR: __file__ is not defined')
-    #     filename = '../data_raw/data_confidential/demand_profile_A_nominal_20180912.csv'
-    data = pd.read_csv(filename)
 
-    ##########################################################################
-    # Read parameter values from data file
-    ##########################################################################
+    file_path_demand_ts = abs_path + cfg['demand_time_series']
+    data = pd.read_csv(file_path_demand_ts)
 
-    filename_param = abs_path + '/data_raw/data_public/parameters_A1.csv'
-    param_df = pd.read_csv(filename_param, index_col=1)  # uses second column for indexing
+    # file_path_weather_ts = abs_path + '/data_preprocessed/' + cfg['weather_time_series']
+    # weather_data = pd.read_csv(file_path_weather_ts)
+
+    file_name_param = cfg['parameters_energy_system']
+    file_path_param = abs_path + file_name_param
+    param_df = pd.read_csv(file_path_param, index_col=1)
     param_value = param_df['value']
 
     ##########################################################################
@@ -253,7 +253,7 @@ def run_model_flexchp(config_path):
 
     if debug:
         filename = os.path.join(
-            helpers.extend_basic_path('lp_files'), 'flexCHB_A1.lp')
+            helpers.extend_basic_path('lp_files'), cfg['filename_lpfile'])
         logging.info('Store lp-file in {0}.'.format(filename))
         model.write(filename, io_options={'symbolic_solver_labels': True})
 
@@ -266,17 +266,7 @@ def run_model_flexchp(config_path):
     energysystem.results['main'] = outputlib.processing.results(model)
     energysystem.results['meta'] = outputlib.processing.meta_results(model)
 
-    energysystem.dump(dpath=abs_path + "/results/optimisation_results/dumps", filename="flexCHB.oemof")
+    energysystem.dump(dpath=abs_path + "/results/optimisation_results/dumps", filename=cfg['filename_dumb'])
 
-stop_time = timeit.default_timer()
-run_time_in_sec = stop_time - start_time
-print("***Run Time***")
-if run_time_in_sec < 60:
-    print("%6.2f" %run_time_in_sec, "seconds")
-elif run_time_in_sec >= 60:
-    run_time_in_min = run_time_in_sec/60
-    residual_seconds = run_time_in_sec%60
-    print("%6.0f" %run_time_in_min, "min,", "%6.0f" %residual_seconds, "s")
-else:
-    print("%12.2f" %run_time_in_sec, "seconds")
+
 
