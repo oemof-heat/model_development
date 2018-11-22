a48b743ff74df8cf23b11508d50ff50ff908d4fb
Jakob Wolf <jakob.wolf@beuth-hochschule.de>
2018-11-22 16:45:48 +0100

    Update main function

--- System_A/src/main.py
+++ System_A/src/main.py
@@ -1,12 +1,48 @@
 
-from oemof.tools import logger
-import logging
+"""
+
+Date: 22nd of November 2018
+Author: Jakob Wolf (jakob.wolf@beuth-hochschule.de)
+
+"""
+
+
 import os
-
 from model_flex_chp import run_model_flexchp
+from preprocessing import preprocess_timeseries
+from plot_and_analyse import analyse_energy_system
+import yaml
+import timeit
 
-experiment_cfg = 'experiment_1.yml'
 
-abs_path = os.path.dirname(os.path.abspath(os.path.join(__file__, '..')))
+def main():
 
-run_model_flexchp(config_path=abs_path + "/experiment_configs/" + experiment_cfg)
+    # Choose configuration file to run model with
+    exp_cfg_file_name = 'experiment_1.yml'
+    config_file_path = os.path.abspath('../experiment_config/' + exp_cfg_file_name)
+    with open(config_file_path, 'r') as ymlfile:
+        cfg = yaml.load(ymlfile)
+
+    start_time = timeit.default_timer()
+
+    if cfg['run_preprocessing']:
+        preprocess_timeseries(config_path=config_file_path)
+    if cfg['run_model']:
+        run_model_flexchp(config_path=config_file_path)
+    if cfg['run_postprocessing']:
+        analyse_energy_system(config_path=config_file_path)
+
+    stop_time = timeit.default_timer()
+    run_time_in_sec = stop_time - start_time
+    print("***Run Time***")
+    if run_time_in_sec < 60:
+        print("%6.2f" % run_time_in_sec, "seconds")
+    elif run_time_in_sec >= 60:
+        run_time_in_min = run_time_in_sec / 60
+        residual_seconds = run_time_in_sec % 60
+        print("%6.0f" % run_time_in_min, "min,", "%6.0f" % residual_seconds, "s")
+    else:
+        print("%12.2f" % run_time_in_sec, "seconds")
+
+
+main()
