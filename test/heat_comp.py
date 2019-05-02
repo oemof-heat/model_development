b5d6613eab0c6f33ce8652d3cc355e1e524f041b
oakca <okanakca@gmail.com>
2019-05-02 15:37:23 +0200

    basic gas-to-elec model is implemented

--- test/heat_comp.py
+++ test/heat_comp.py
@@ -1,11 +1,57 @@
-from oemof import solph
+from oemof import solph, outputlib
+from oemof.network import Node
+import pandas as pd
 
 
-def create_model(i):
+def create_model(data, timesteps):
     """
     A basic model for new heat components
     """
-    return i
+    # Adjust Timesteps
+    timesteps = pd.date_range('1/1/2019', periods=timesteps, freq='H')
+
+    # Create Energy System
+    es = solph.EnergySystem(timeindex=timesteps)
+    Node.registry = es
+
+    # Data Manipulation
+    data = data
+
+    # Create Busses
+    b_gas = solph.Bus(label='b_gas')
+    b_elec = solph.Bus(label='b_elec')
+
+    # Create Sources
+    s_gas = solph.Source(label='s_gas',
+                         outputs={b_gas: solph.Flow(
+                             nominal_value=200)})
+
+    # Create Sink
+    demand = solph.Sink(label='demand',
+                        inputs={b_elec: solph.Flow(
+                            actual_value=100,
+                            fixed=True,
+                            nominal_value=1)})
+
+    # Create Transformer
+    t_gas = solph.Transformer(label='t_gas',
+                              inputs={b_gas: solph.Flow()},
+                              outputs={b_elec: solph.Flow(
+                                  variable_costs=50)},
+                              conversion_factors={b_elec: 0.5})
+
+    # Create Model
+    m = solph.Model(es)
+
+    # Solve Model
+    m.solve(solver='cbc', solve_kwargs={'tee': False})
+
+    # Save Results
+    es.results['main'] = outputlib.processing.results(m)
+    es.results['meta'] = outputlib.processing.meta_results(m)
+    es.dump(dpath=None, filename=None)
+
+    return m
 
 
 # Component Link as Example
@@ -33,5 +79,23 @@ def create_model(i):
 """
 
 if __name__ == '__main__':
-    model = create_model(None)
-    print(model)
+    # Input Data & Timesteps
+    data = None
+    timesteps = 10
+
+    # Create & Solve Model
+    model = create_model(data, timesteps)
+
+    # Get Results
+    es = solph.EnergySystem()
+    es.restore(dpath=None, filename=None)
+
+    # Show Results
+    b_gas = outputlib.views.node(es.results['main'], 'b_gas')
+    b_elec = outputlib.views.node(es.results['main'], 'b_elec')
+
+    print('-----------------------------------------------------')
+    print('Bus Gas\n', b_gas['sequences'])
+    print('-----------------------------------------------------')
+    print('Bus Elec\n', b_elec['sequences'])
+    print('-----------------------------------------------------')
