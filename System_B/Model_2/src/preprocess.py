c3393a3d9147e2083b47443931333f3d13243bb5
jnnr <32454596+jnnr@users.noreply.github.com>
2019-08-07 16:30:18 +0200

    Correct for nan

--- System_B/Model_2/src/preprocess.py
+++ System_B/Model_2/src/preprocess.py
@@ -40,8 +40,8 @@ def prepare_timeseries_price_electricity(input_filename, output_filename):
     r"""
     Prepare electricity price time series
     """
-    electricity_spot_price = pd.read_csv(input_filename, sep=';', decimal=',')
-    electricity_spot_price = electricity_spot_price['Deutschland/Österreich/Luxemburg[Euro/MWh]'].rename('electricity_spot_price')
+    electricity_spot_price = pd.read_csv(input_filename, sep=';', decimal=',', na_values='-')
+    electricity_spot_price = electricity_spot_price['Deutschland/Österreich/Luxemburg[Euro/MWh]'].rename('price_electricity_spot')
     if not os.path.exists(os.path.dirname(output_filename)):
         os.makedirs(os.path.dirname(output_filename))
     electricity_spot_price.to_csv(output_filename, header=True)
@@ -60,7 +60,7 @@ def prepare_timeseries_demand_heat(year, parameter_bdew,
     # create a DataFrame to hold the timeseries
     demand = pd.DataFrame(index=temperature.index)
     for component, parameter_list in parameter_bdew.items():
-        print(component)
+        # print(component)
         component_demand = pd.DataFrame()
         for item in parameter_list:
             timeseries_demand = bdew.HeatBuilding(demand.index,
@@ -70,8 +70,8 @@ def prepare_timeseries_demand_heat(year, parameter_bdew,
                                                   **item).get_bdew_profile()
             component_demand[item['shlp_type']] = timeseries_demand
         demand[component] = component_demand.sum(axis=1)
-    print(output_filename)
-    print(demand.head())
+    # print(output_filename)
+    # print(demand.head())
     if not os.path.exists(os.path.dirname(output_filename)):
         os.makedirs(os.path.dirname(output_filename))
     demand.to_csv(output_filename)
