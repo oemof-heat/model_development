7379a2be297e7f8c07acf73ca25f78c2c17ecf0b
jnnr <jann.launer@rl-institut.de>
2020-06-03 17:57:58 +0200

    Prepare timeseries

--- System_B/Model_3/scripts/preprocessing.py
+++ System_B/Model_3/scripts/preprocessing.py
@@ -1,14 +1,45 @@
 import logging
 import os
 
+import pandas as pd
+
 from oemof.tools.logger import define_logging
 from oemof.tabular.datapackage import building
 
 from helper import get_experiment_dirs
 
 
-def preprocess_data():
-    pass
+TIMEINDEX = pd.date_range('1/1/2017', periods=8760, freq='H')
+
+
+def prepare_electricity_price_profiles(raw_price, destination):
+    def save(df, name):
+        df.to_csv(os.path.join(destination, name))
+
+    raw_price = pd.read_csv(raw_price, index_col=0)
+
+    marginal_cost_profile = raw_price['price_electricity_spot']
+    marginal_cost_profile.index = TIMEINDEX
+    marginal_cost_profile.index.name = 'timeindex'
+    marginal_cost_profile.name = 'electricity-selling'
+    save(marginal_cost_profile, 'marginal_cost_profile.csv')
+
+    carrier_cost_profile = marginal_cost_profile.copy()
+    carrier_cost_profile.name = 'electricity-buying'
+    save(carrier_cost_profile, 'carrier_cost_profile.csv')
+
+
+def prepare_heat_demand_profile(heat_demand_profile, destination):
+    def save(df, name):
+        df.to_csv(os.path.join(destination, name))
+
+    heat_demand_profile = pd.read_csv(heat_demand_profile, index_col='timestamp')
+    heat_demand_profile = heat_demand_profile.sum(axis=1)
+    heat_demand_profile.index = TIMEINDEX
+    heat_demand_profile.index.name = 'timeindex'
+    heat_demand_profile.name = 'heat-demand-01'
+
+    save(heat_demand_profile, 'heat-demand_profile.csv')
 
 
 def infer_metadata(name, preprocessed):
@@ -50,7 +81,16 @@ def main():
 
     dirs = get_experiment_dirs()
 
-    preprocess_data()
+    prepare_heat_demand_profile(
+        os.path.join(dirs['raw'], 'demand_heat_2017.csv'),
+        os.path.join(dirs['preprocessed'], 'data', 'sequences')
+    )
+
+    prepare_electricity_price_profiles(
+        os.path.join(dirs['raw'], 'price_electricity_spot_2017.csv'),
+        os.path.join(dirs['preprocessed'], 'data', 'sequences')
+    )
+
     infer_metadata('name', dirs['preprocessed'])
 
 
