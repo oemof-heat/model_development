c4cd46b3bf55785945dd2f09bf7b7d7bdb5371ee
jnnr <jann.launer@rl-institut.de>
2020-06-04 09:47:31 +0200

    Set price components in preprocessing

--- System_B/Model_3/scripts/preprocessing.py
+++ System_B/Model_3/scripts/preprocessing.py
@@ -21,13 +21,38 @@ def prepare_investment_cost(fix_costs_raw_file):
         + fix_cost.apply(lambda x: annuity(x['overnight_cost'], x['lifetime'], wacc), axis=1)
 
 
+def get_elements(dir):
+    elements = {}
 
-def prepare_electricity_price_profiles(raw_price, destination):
+    for file in os.listdir(dir):
+        key = file.split('.')[0]
+
+        value = pd.read_csv(os.path.join(dir, file), index_col='name', delimiter=';')
+
+        elements.update({key: value})
+
+    return elements
+
+
+def save_elements(elements, dir):
+    for key, value in elements.items():
+        filename = os.path.join(dir, key + '.csv')
+        value.to_csv(filename, sep=';')
+
+
+def set_gas_price(gas_price, elements_dir):
+    elements = get_elements(elements_dir)
+
+    elements['gas-chp']['carrier_cost'] = gas_price
+
+    elements['gas-hob']['carrier_cost'] = gas_price
+
+    save_elements(elements, elements_dir)
+
+
+def prepare_electricity_price_profiles(charges_tax_levies_el, chp_surcharge, raw_price, destination):
     def save(df, name):
         df.to_csv(os.path.join(destination, name))
-
-    charges_tax_levies_el = 93
-    chp_surcharge = 0
 
     raw_price = pd.read_csv(raw_price, index_col=0)
 
@@ -98,11 +123,19 @@ def infer_metadata(name, preprocessed):
 def main():
     print('Preprocessing')
 
+    gas_price = 35
+    charges_tax_levies_el = 93
+    chp_surcharge = 0
+
     dirs = get_experiment_dirs()
 
     prepare_investment_cost(
         os.path.join(dirs['raw'], 'fix_cost_assumptions.csv')
     )
+
+    elements_dir = os.path.join(dirs['preprocessed'], 'data', 'elements')
+
+    set_gas_price(gas_price, elements_dir)
 
     prepare_heat_demand_profile(
         os.path.join(dirs['raw'], 'demand_heat_2017.csv'),
@@ -110,6 +143,8 @@ def main():
     )
 
     prepare_electricity_price_profiles(
+        charges_tax_levies_el,
+        chp_surcharge,
         os.path.join(dirs['raw'], 'price_electricity_spot_2017.csv'),
         os.path.join(dirs['preprocessed'], 'data', 'sequences')
     )
