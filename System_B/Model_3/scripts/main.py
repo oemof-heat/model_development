b863186ad4b3d258cd6fe5286d676b64e8c2023b
jnnr <jann.launer@rl-institut.de>
2020-06-15 15:57:37 +0200

    Loop through all scenarios

--- System_B/Model_3/scripts/main.py
+++ System_B/Model_3/scripts/main.py
@@ -1,3 +1,4 @@
+import helper
 import preprocessing
 import optimization
 import postprocessing
@@ -5,7 +6,15 @@ import plotting
 
 
 if __name__ == '__main__':
-    preprocessing.main()
-    optimization.main()
-    postprocessing.main()
-    plotting.main()
+    scenario_assumptions = helper.get_scenario_assumptions()
+
+    for i in scenario_assumptions.index:
+        print(f"Running scenario {i} with the name '{scenario_assumptions.loc[i]['name']}'")
+
+        preprocessing.main(**scenario_assumptions.loc[i])
+
+        optimization.main(**scenario_assumptions.loc[i])
+
+        postprocessing.main(**scenario_assumptions.loc[i])
+
+        plotting.main(**scenario_assumptions.loc[i])
