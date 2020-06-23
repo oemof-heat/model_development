7755803ced9b9640c934d7836a1312d47ad0e444
jnnr <jann.launer@rl-institut.de>
2020-06-23 14:15:46 +0200

    Allow to select which scenarios are run

--- System_B/Model_3/scripts/main.py
+++ System_B/Model_3/scripts/main.py
@@ -8,7 +8,9 @@ import plotting
 if __name__ == '__main__':
     scenario_assumptions = helper.get_scenario_assumptions()
 
-    for i in scenario_assumptions.index:
+    selected_id = scenario_assumptions.index
+
+    for i in selected_id:
         print(f"Running scenario {i} with the name '{scenario_assumptions.loc[i]['name']}'")
 
         preprocessing.main(**scenario_assumptions.loc[i])
